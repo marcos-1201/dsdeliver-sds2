c665668a6ed4fbf9cdf8751be278ad9a9ec92216
marcos-1201 <mvi.santiago@gmail.com>
2021-01-07 23:22:34 -0300

    Add ProductCard and ProductsList

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,23 @@
-import StepsHeaders from './StepsHeader';
+import { useEffect, useState } from 'react';
 import './styles.css';
+import StepsHeaders from './StepsHeader';
+import ProductsList from './ProductsList';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders(){
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeaders />
+            <ProductsList products={products} />
         </div>
     )
 }
